895ae6031c4cf8a97457f69beae33feb0c983358
LouisCastricato <louis_castricato@brown.edu>
2022-09-22 01:14:33 +0000

    best of n sampling works

--- audience/model/utils.py
+++ audience/model/utils.py
@@ -1,7 +1,8 @@
+import torch
 import transformers
+from tqdm import tqdm
 
-
-def best_of_n(model, tokenizer, prompt, n=50, mbs=5):
+def best_of_n(model, tokenizer, prompt, n=40, mbs=10, use_tqdm=False):
     """Returns the best of n samples from a model
     :param model: A Huggingface model
     :param tokenizer: A Huggingface tokenizer
@@ -12,7 +13,7 @@ def best_of_n(model, tokenizer, prompt, n=50, mbs=5):
 
     # first tokenize the prompt
     prompt = tokenizer(prompt, return_tensors="pt")
-    print(prompt)
+
     # extract input_ids and attention_mask
     input_ids = prompt.input_ids
     attn_mask = prompt.attention_mask
@@ -20,14 +21,20 @@ def best_of_n(model, tokenizer, prompt, n=50, mbs=5):
     # and stack n times
     input_ids = input_ids.repeat(mbs, 1).to(model.device)
     attn_mask = attn_mask.repeat(mbs, 1).to(model.device)
-    print(input_ids.shape)
-    print(attn_mask.shape)
+
 
     # save a giant list of output_ids
     output_ids = []
     output_scores = []
+
+    # iterate over the number of samples
+    if use_tqdm:
+        iterator = tqdm(range(n//mbs))
+    else:
+        iterator = range(n//mbs)
+        
     # now generate. make sure that we utilize our mbs
-    for i in range(n // mbs):
+    for i in iterator:
         # generate
         out_temp = model.generate(
             input_ids,
@@ -38,22 +45,45 @@ def best_of_n(model, tokenizer, prompt, n=50, mbs=5):
             top_k=60,
             return_dict_in_generate=True,
             output_scores=True,
+            pad_token_id=50256,
         )
+        # get the length of input_ids
+        start_idx = input_ids.shape[1]
 
-        # append to our list
-        output_ids.append(out_temp.sequences)
-        output_scores.append(out_temp.scores)
+        # compute perplexity
+        log_probs = []
+        for i, t in enumerate(out_temp.scores):
+            # log softmax
+            t = -torch.log_softmax(t, dim=-1)[:, out_temp.sequences[:, start_idx+i]][0]
+            # replace -inf with 0
+            t[t == float("inf")] = 0
 
+            log_probs.append(t)
+        # save input_ids
+        output_ids += out_temp.sequences.tolist()
+
+        # stack log probs
+        log_probs = torch.stack(log_probs, dim=1).sum(dim=1)
+        output_scores += log_probs.tolist()
+
+    # zip for sorting
     zipped = zip(output_ids, output_scores)
     # sort by score in ascending order
     zipped = sorted(zipped, key=lambda x: x[1], reverse=True)
+
+    # unzip
+    output_ids, output_scores = zip(*zipped)
+
     # and return the best one
-    return tokenizer.decode(zipped[0][0], skip_special_tokens=True)
+    return tokenizer.decode(torch.tensor(output_ids[0]), skip_special_tokens=True)
 
 if __name__ == "__main__":
     # load the model and tokenizer
-    model = transformers.AutoModelForCausalLM.from_pretrained("gpt2").to("cuda")
-    tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2")
-
+    model = transformers.AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-j-6B').to("cuda").int8()
+    tokenizer = transformers.AutoTokenizer.from_pretrained('EleutherAI/gpt-j-6B')
+    prompt = """Below is a conversation between two people, Alice and Bob. Alice and Bob want to make small talk.
+A: Hi Bob!
+B: How are you doing?
+"""
     # now generate a response
-    print(best_of_n(model, tokenizer, "Hello, my name is"))
+    print(best_of_n(model, tokenizer, prompt, use_tqdm=True))
